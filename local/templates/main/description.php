b57d86ea7f28c9ef41afca1fc54e848ae0542adb
Anton Travkin <travkin.rh@gmail.com>
2024-05-05 13:31:46 +0300

    bitrix local

---
+++ local/templates/main/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'Осноыной шаблон',
+	'DESCRIPTION' => 'Осноыной шаблон для сайта'
+);
+?>
